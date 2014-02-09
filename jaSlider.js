dd0f0d9c2616059d8205cd89df56a252faaeb29d
juananthony <juanantoniojimenez@me.com>
2014-02-09 21:10:51 +0100

    Initial commit
    initial commit with js file

---
+++ jaSlider.js
@@ -0,0 +1,12 @@
+/*
+ *	jaSlider
+ *	
+ *	jQuery plugin to create a menu slider.
+ *
+ */
+$.fn.jaSlider = function () {
+	var slider = this; 
+	slider.on('click.jaslide','button',function() {
+		
+	});
+}
