49fb77e9e906780aef6e091bde4702dcb8f92d3a
juananthony <juanantoniojimenez@me.com>
2014-02-10 19:55:08 +0100

    jaSlider v-1.0 ready
    jaSlider in v-1.0

--- js/jaSlider.coffee
+++ js/jaSlider.coffee
@@ -1,15 +1,21 @@
+ #
+ #	jaSlider
+ #	
+ #	jQuery plugin to create a menu slider.
+ #
+ #
  $.fn.jaSlider = (options) ->
- 	@each() ->
+ 	@.each ->
  		settings = $.extend
- 			button: $ 'button.jaslidder-button'
+ 			button: $ 'button.jaslider-button'
  			menuDiv: $(".jaslider-menu")
  			contentDiv: $(".jaslider-content")
  			options
 
  		margin = settings.contentDiv.get(0).style.marginLeft;
-
  		settings.button.click ->
  			settings.menuDiv.animate
- 				left: if parseInt(settings.menuDiv.css('left'),10) == 0 then settings.menuDiv.outerWidth() else 0
+ 				left: if parseInt(settings.menuDiv.css('left'),10) == 0 then -settings.menuDiv.outerWidth() else 0
  			settings.contentDiv.animate
- 				'margin-left': if margin == '20%' then '0%' else '20%'
+ 				left: if parseInt(settings.menuDiv.css('left'),10) == 0 then 0 else settings.menuDiv.outerWidth()
+ 				#'margin-left': if margin == '20%' then '0%' else '20%'
--- js/jaSlider.js
+++ js/jaSlider.js
@@ -1,19 +1,19 @@
 $.fn.jaSlider = function(options) {
-    return this.each()(function() {
-      var margin, settings;
-      settings = $.extend({
-        button: $('button.jaslidder-button'),
-        menuDiv: $(".jaslider-menu"),
-        contentDiv: $(".jaslider-content")
-      }, options);
-      margin = settings.contentDiv.get(0).style.marginLeft;
-      return settings.button.click(function() {
-        settings.menuDiv.animate({
-          left: parseInt(settings.menuDiv.css('left'), 10) === 0 ? settings.menuDiv.outerWidth() : 0
-        });
-        return settings.contentDiv.animate({
-          'margin-left': margin === '20%' ? '0%' : '20%'
-        });
+  return this.each(function() {
+    var margin, settings;
+    settings = $.extend({
+      button: $('button.jaslider-button'),
+      menuDiv: $(".jaslider-menu"),
+      contentDiv: $(".jaslider-content")
+    }, options);
+    margin = settings.contentDiv.get(0).style.marginLeft;
+    return settings.button.click(function() {
+      settings.menuDiv.animate({
+        left: parseInt(settings.menuDiv.css('left'), 10) === 0 ? -settings.menuDiv.outerWidth() : 0
+      });
+      return settings.contentDiv.animate({
+        left: parseInt(settings.menuDiv.css('left'), 10) === 0 ? 0 : settings.menuDiv.outerWidth()
       });
     });
-  };
+  });
+};
