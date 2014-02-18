3a666111e177c5e222e625ec27f6236b9149857d
juananthony <juanantoniojimenez@me.com>
2014-02-18 20:03:19 +0100

    Added new settings options to define menuDiv.

--- js/jaSlider.coffee
+++ js/jaSlider.coffee
@@ -10,12 +10,21 @@
  			button: $ 'button.jaslider-button'
  			menuDiv: $(".jaslider-menu")
  			contentDiv: $(".jaslider-content")
+ 			menuWidth: 250
  			options
+ 		
+ 		# Setting the width and left to the menu
+ 		settings.menuDiv.css('width', settings.menuWidth)
+ 		settings.menuDiv.css('left', -settings.menuWidth)
 
- 		margin = settings.contentDiv.get(0).style.marginLeft;
+ 		# Click event handler
  		settings.button.click ->
+ 			settings.slide()
+
+ 		### Function to slide menu ###
+ 		# Animate menuDiv and contentDiv to show the menu
+ 		settings.slide = ->
  			settings.menuDiv.animate
  				left: if parseInt(settings.menuDiv.css('left'),10) == 0 then -settings.menuDiv.outerWidth() else 0
  			settings.contentDiv.animate
  				left: if parseInt(settings.menuDiv.css('left'),10) == 0 then 0 else settings.menuDiv.outerWidth()
- 				#'margin-left': if margin == '20%' then '0%' else '20%'
--- js/jaSlider.js
+++ js/jaSlider.js
@@ -1,19 +1,26 @@
 $.fn.jaSlider = function(options) {
   return this.each(function() {
-    var margin, settings;
+    var settings;
     settings = $.extend({
       button: $('button.jaslider-button'),
       menuDiv: $(".jaslider-menu"),
-      contentDiv: $(".jaslider-content")
+      contentDiv: $(".jaslider-content"),
+      menuWidth: 250
     }, options);
-    margin = settings.contentDiv.get(0).style.marginLeft;
-    return settings.button.click(function() {
+    settings.menuDiv.css('width', settings.menuWidth);
+    settings.menuDiv.css('left', -settings.menuWidth);
+    settings.button.click(function() {
+      return settings.slide();
+    });
+    /* Function to slide menu*/
+
+    return settings.slide = function() {
       settings.menuDiv.animate({
         left: parseInt(settings.menuDiv.css('left'), 10) === 0 ? -settings.menuDiv.outerWidth() : 0
       });
       return settings.contentDiv.animate({
         left: parseInt(settings.menuDiv.css('left'), 10) === 0 ? 0 : settings.menuDiv.outerWidth()
       });
-    });
+    };
   });
 };
